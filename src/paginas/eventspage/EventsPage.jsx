465d8b5947d92df31e893f1e478d09fbea1f7f74
Ary123-start <arianneclaudia@gmail.com>
2026-01-02 15:44:02 -0300

    aqui adicionei mais um card

--- src/paginas/eventspage/EventsPage.jsx
+++ src/paginas/eventspage/EventsPage.jsx
@@ -2,20 +2,21 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "../../components/button/Button";
 import "./EventsPage.css";
-import { Img } from "../../assets/imagemevento.png";
+import Img from "../../assets/imagemevento.png";
 import { EventCard } from "../../components/eventcard/EventCard";
 import { FilterBar } from "../../components/filterbar/FilterBar";
-import { Pagination } from "../../components/pagination/pagination";
+import { Pagination } from "../../components/pagination/Pagination";
 import { Header } from "../../components/header/Header";
-import { footer } from "../../components/footer/Footer";
+import Footer from "../../components/footer/footer";
 
 export function EventsPage() {
     const [currentPage, setCurrentPage] = useState(1);
+    const navigation = useNavigate();
 
     const handlePageChange = (page) => {
         setCurrentPage(page);
     };
-const navigation = useNavigate();
+
     return (
         <div className="events-page">
             <Header />
@@ -30,9 +31,12 @@ const navigation = useNavigate();
                     <EventCard image={Img} category="Cultural" title="São João" date="2025-06-20" location="Igarassu" />
                     <EventCard image={Img} category="Musical" title="Show de Verão" date="2025-02-10" location="Igarassu" />
                     <EventCard image={Img} category="Gastronomia" title="Festival Gastronômico" date="2025-08-15" location="Igarassu" />
+                    <EventCard image={Img} category="Tradicional" title="Carnaval 2025" date="2025-02-25" location="Igarassu" />
+
                 </div>
                 <Pagination currentPage={currentPage} totalPages={1} onPageChange={handlePageChange} />
             </main>
+            <Footer />
         </div>
     );
 }
