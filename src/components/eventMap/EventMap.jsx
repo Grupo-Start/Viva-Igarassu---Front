30fb966d658de9db898708708b6e4530c83090e5
Ary123-start <arianneclaudia@gmail.com>
2026-01-02 18:23:09 -0300

    ajustes finalizado da página de informações de evento

--- src/components/eventMap/EventMap.jsx
+++ src/components/eventMap/EventMap.jsx
@@ -6,14 +6,6 @@ export function EventMap() {
         <div className="event-map">
             <h3>Mapa</h3>
             <img src={imgMapa} alt="Mapa de Igarassu" className="event-map-image" />
-            <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!..."
-                width="100%"
-                height="300"
-                style={{ border: 0 }}
-                allowFullScreen=""
-                loading="lazy"
-            ></iframe>
         </div>
     );
 }
--- src/components/mainEvent/MainEvent.jsx
+++ src/components/mainEvent/MainEvent.jsx
@@ -6,7 +6,10 @@ import imgCarnaval from "../../assets/imgcarnaval.png";
 export function MainEvent() {
     return (<section className="main-event">
         <div className="main-event-container">
-            <img src={imgCarnaval} alt="Carnaval de Igarassu - festa tradicional com frevo, maracatu e programação cultural" className="main-event-image" /> <div className="main-event-text"> <h3>Carnaval</h3> <p><strong>Data:</strong> 14 - 17 de Fevereiro 2026</p> <p> O Carnaval de Igarassu (PE) é marcado pela tradição e pela cultura local. A festa reúne frevo, maracatu, coco de roda e outras
+            <img src={imgCarnaval} alt="Carnaval de Igarassu - festa tradicional com frevo, maracatu e programação cultural" className="main-event-image" /> 
+            <div className="main-event-text"> <h3>Carnaval</h3> 
+            <p><strong>Data:</strong> 14 - 17 de Fevereiro 2026</p> 
+            <p> O Carnaval de Igarassu (PE) é marcado pela tradição e pela cultura local. A festa reúne frevo, maracatu, coco de roda e outras
                 atrações típicas, sempre valorizando artistas da cidade e homenageando figuras importantes. Os
                 eventos acontecem em vários polos, especialmente no Sítio Histórico, e incluem blocos,
                 concursos e programação infantil. É um carnaval acolhedor, organizado e seguro, conhecido
